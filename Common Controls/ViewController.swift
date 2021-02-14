cfcbfd76f2f4e46002df29cca8603799f0da22e8
Jon Burnett <74561172+JB3991@users.noreply.github.com>
2021-02-14 17:53:20 +0000

    Updated Code
    Added Comments to the code

--- Common Controls/ViewController.swift
+++ Common Controls/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // Outlets - Link Ui from the Storyboard to the ViewController
     @IBOutlet weak var toggle: UISwitch!
     @IBOutlet weak var slider: UISlider!
     
@@ -16,22 +17,27 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    // Tap Function to find and print the tap location
     @IBAction func respondToTapGesture(_ sender: UITapGestureRecognizer) {
+        // find the location of the users tap and print this to the log
         let location = sender.location(in: view)
         print(location)
     }
     
+    // Button Presses
     @IBAction func buttonTapped(_ sender: Any) {
         print("Button was tapped")
-        
+        // look to see if the toggle is on or off and print the following if so.
             if toggle.isOn {
                 print("The switch is on!")
             } else {
                 print("The switch is off.")
             }
+        // print the sliders value
             print("The slider is set to \(slider.value)")
     }
     
+    // print if the toggle is on or off
     @IBAction func switchToggled(_ sender: UISwitch) {
         if sender.isOn{
             print("The switch is on")
@@ -40,16 +46,19 @@ class ViewController: UIViewController {
         }
     }
     
+    //print the sliders value
     @IBAction func sliderValueChanged(_ sender: UISlider) {
         print(sender.value)
     }
     
+    // when the retun key is pressed on the keyboard, print the text in the text field
     @IBAction func keyboardReturnKeyTapped(_ sender: UITextField) {
         if let text = sender.text {
             print(text)
         }
     }
     
+    // prints every leter typed in the text field to the log
     @IBAction func textChanged(_ sender: UITextField) {
         if let text = sender.text {
             print(text)
